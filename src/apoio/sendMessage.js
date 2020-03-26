13065bc48ef5a2f7eb2a8a05a62880d102a29495
Ziro VI <ahmad.forhat@gmail.com>
2020-03-26 14:41:33 -0300

    arrumando a env

--- src/apoio/sendMessage.js
+++ src/apoio/sendMessage.js
@@ -1,13 +1,13 @@
 require('dotenv').config() 
-const accountSid = process.accountSid 
-const authToken = process.authToken
+const accountSid = process.env.accountSid 
+const authToken = process.env.authToken
 const client = require('twilio')(accountSid, authToken)
 
 const enviarMensagem = (numberTo,body) => {
     client.messages 
         .create({ 
             body: body, 
-            from: `+${process.numero_ziro}`,       
+            from: `whatsapp:+${process.env.numero_ziro_sand}`,     
             to: `whatsapp:+${numberTo}`
         }) 
         .then(message => console.log(message.sid)) 
