0be6d4010c74bc7698e35f9c2fd24289cc32f3ef
Ziro VI <ahmad.forhat@gmail.com>
2020-03-26 14:42:19 -0300

    condicional do body

--- src/lambdas/sendWhats.js
+++ src/lambdas/sendWhats.js
@@ -2,24 +2,22 @@ const sendMessage = require('../apoio/sendMessage')
 const main = require('../templates/main')
 require('dotenv').config()
 
-const sendWhats = async () => {
+const sendWhats = async (event) => {
     try {
-        const corpo = event.body
-        const {destinatario, mensagem} = corpo
-        if(destinatario && mensagem){
-            const enviar = sendMessage(destinatario, mensagem)
-            console.log(event.body)
+        if(event.body.destinatario && event.body.mensagem){
+            const enviar = sendMessage(event.body.destinatario, event.body.mensagem)
             return {
                 statusCode: 200,
                 body: JSON.stringify(enviar)
             }
         }else{
             return {
-                statusCode:412,
-                body: JSON.stringify({erro:404, mensagem:'Requisição feita sem destinatario ou mensagem'})
+                statusCode: 412,
+                body: JSON.stringify('Body da requisição não encontrada.')
             }
         }
     }catch (error) {
+        console.log(bodyObject.destinatario)
         return {
             statusCode: 500,
             body: JSON.stringify(error)
