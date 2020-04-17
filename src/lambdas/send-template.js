58ebba74c3e2d617d205055fa25a25bf9ba78301
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-17 09:24:23 -0700

    remove middy/validator, was doing nothing

--- src/lambdas/send-template.js
+++ src/lambdas/send-template.js
@@ -1,14 +1,9 @@
 const middy = require('@middy/core')
 const jsonBodyParser = require('@middy/http-json-body-parser')
-const validator = require('@middy/validator')
 const httpErrorHandler = require('@middy/http-error-handler')
 const { basicAuth } = require('@ziro/middleware')
-const inputValidator = require('../utils/inputValidator')
 const twilioTemplates = require('../utils/twilioTemplates')
 const sendTwilio = require('../utils/sendTwilio')
-
-console.log(basicAuth)
-console.log(inputValidator)
 
 const sendTemplate = async event => {
   const { recipient, template_name, template_parameters } = event.body
@@ -25,7 +20,6 @@ const sendTemplate = async event => {
 const handler = middy(sendTemplate)
   .use(basicAuth) // authorizes request using Ziro's own token
   .use(jsonBodyParser()) // parses the request body when it's a JSON and converts it to an object
-  .use(validator(inputValidator)) // validates the input
   .use(httpErrorHandler()) // handles common http errors and returns proper responses
 
 module.exports = { handler }
