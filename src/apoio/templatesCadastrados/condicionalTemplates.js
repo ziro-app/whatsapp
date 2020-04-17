4c920dd3715daed5b4ec23740b599f853928472a
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-16 18:52:52 -0700

    clear repository

--- src/apoio/templatesCadastrados/condicionalTemplates.js
+++
@@ -1,7 +0,0 @@
-const notificacaoLead = require('./notificacaoLead')
-
-const condicional = (modelo,parametros) => {
-    if(modelo === 'Notificação Lead') return notificacaoLead(parametros)
-}
-
-module.exports = condicional
