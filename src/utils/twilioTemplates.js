927d38d8ba075c8ff05cf850d0e40ef3f116fcb6
João L. S. Silva <joao.silva@easysoft.com.br>
2020-09-17 15:37:41 -0300

    update antifraud templates

--- src/utils/twilioTemplates.js
+++ src/utils/twilioTemplates.js
@@ -1,20 +1,21 @@
 const twilioTemplates = (name, params) => {
 	// validate template params
-	if (!params) throw { statusCode: 400, body: 'Request is missing template_parameters' }
-	if (name === 'lead-registered')
+	if (!params) throw { statusCode: 400, body: "Request is missing template_parameters" }
+	if (name === "lead-registered")
 		if (!params.name || !params.whats)
 			throw { statusCode: 400, body: `Invalid template_parameters for template: '${name}'` }
-	if (name === 'antifraud-trouble')
-		if (!params.razao)
+	if (name === "antifraud-trouble")
+		if (!params.razao || !params.errorName)
 			throw { statusCode: 400, body: `Invalid template_parameters for template: '${name}'` }
+	if (name === "antifraud-manual-approval")
+		if (!params.razao) throw { statusCode: 400, body: `Invalid template_parameters for template: '${name}'` }
 	// match template name to its expected parameters
 	const templates = {
-		'lead-registered': 
-			`Lead ${params.name} cadastrou-se no catálogo com o whatsapp ${params.whats}`,
-		'antifraud-trouble':
-			`Lead ${params.razao} precisa de suporte no antifraude`
+		"lead-registered": `Lead ${params.name} cadastrou-se no catálogo com o whatsapp ${params.whats}`,
+		"antifraud-trouble": `Lead ${params.razao} precisa de suporte no antifraude devido ao erro ${params.errorName}`,
+		"antifraud-manual-approval": `Lead ${params.razao} está aguardando aprovação manual no antifraude`,
 	}
-	if (!templates[name]) throw { statusCode: 400, body: 'Invalid template_name' }
+	if (!templates[name]) throw { statusCode: 400, body: "Invalid template_name" }
 	return templates[name]
 }
 
